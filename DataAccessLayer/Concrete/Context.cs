e1122bff555bae29a5c48ef129f5fd22691fbbff
Buğra BAYRAK <bugrabayrak@hotmail.com>
2021-09-21 11:02:04 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseMySql("server=localhost;port=3306;database=mjyblog;uid=root;password=12345678;", new MySqlServerVersion(new Version(8, 0,11)));
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ MJYBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MJYBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
